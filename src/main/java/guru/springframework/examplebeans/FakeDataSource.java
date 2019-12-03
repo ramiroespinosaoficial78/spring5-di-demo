e9764572c0763c8261d643231056f0f2df479bfc
ramiro.espinosa.oficial <ramiro.espinosa.oficial@gmail.com>
2019-12-03 16:50:08 -0600

    Getting properties using @Value and @PropertySource

---
+++ src/main/java/guru/springframework/examplebeans/FakeDataSource.java
@@ -0,0 +1,33 @@
+package guru.springframework.examplebeans;
+
+public class FakeDataSource {
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDburl() {
+        return dburl;
+    }
+
+    public void setDburl(String dburl) {
+        this.dburl = dburl;
+    }
+
+    private String username;
+    private String password;
+    private String dburl;
+
+}
